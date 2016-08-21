1ef4253621adae79e9b5a021bae480194255142d
Michael Pratt <pratt@hablarmierda.net>
2016-08-20 19:36:01 -0500

    Install docker-compose on the local bin, rather than using a slackware package It'ś just not worth the hussle.

--- setup.sh
+++ setup.sh
@@ -27,7 +27,7 @@ if [ -w "/etc/" ]; then
 fi
 
 if ! [ -e "${HOME}/.bin/psysh" ]; then
-    echo "Installing physh"
+    echo "Installing psysh"
     wget http://psysh.org/psysh -O ~/.bin/psysh
     chmod +x ~/.bin/psysh
     echo ""
@@ -37,6 +37,14 @@ if ! [ -e "${HOME}/.bin/phpunit" ]; then
     echo "Installing phpunit"
     wget https://phar.phpunit.de/phpunit.phar -O ~/.bin/phpunit
     chmod +x ~/.bin/phpunit
+    echo ""
+fi
+
+if ! [ -e "${HOME}/.bin/docker-compose" ]; then
+    DCVERSION="1.8.0"
+    echo "Installing docker-compose ${DCVERSION}"
+    curl -L "https://github.com/docker/compose/releases/download/${DCVERSION}/docker-compose-$(uname -s)-$(uname -m)" > ${HOME}/.bin/docker-compose
+    chmod +x ${HOME}/.bin/docker-compose
     echo ""
 fi
 
