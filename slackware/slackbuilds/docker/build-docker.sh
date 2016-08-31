947f4abb8717f37b136d718ce14a967ea5deacec
Michael Pratt <pratt@hablarmierda.net>
2016-08-31 13:59:33 -0500

    Build slackware docker pkgs only when they are not already installed

--- slackware/slackbuilds/docker/build-docker.sh
+++ slackware/slackbuilds/docker/build-docker.sh
@@ -5,9 +5,14 @@ CWD=$(dirname $0)
 SLACKBUILDS=( 'godep/godep.SlackBuild' 'runc/runc.SlackBuild' 'containerd/containerd.SlackBuild' 'docker/docker.SlackBuild' 'docker-compose/docker-compose.SlackBuild')
 [[ "$(whoami)" != "root" ]] && echo "You need to be root in order to run this script" && exit 1
 for package in ${SLACKBUILDS[@]}; do
+    pkg=$(basename ${package} | sed 's/\.SlackBuild//g')
     ! [ -d "${CWD}/$(dirname ${package})" ] && echo "${package} not found" && continue
-    cd ${CWD}/$(dirname ${package})
-    echo "Building $(basename ${package})"
-    bash $(basename ${package})
-    upgradepkg --install-new --reinstall /tmp/$(basename ${package} | sed 's/\.SlackBuild//g')*.t?z
+    if [ -z $(ls /var/log/packages/ | egrep -i "^${pkg}-") ]; then
+        cd ${CWD}/$(dirname ${package})
+        echo "Building ${pkg}"
+        bash $(basename ${package})
+        upgradepkg --install-new /tmp/${pkg}*.t?z
+    else
+        echo "${pkg} already installed"
+    fi
 done
