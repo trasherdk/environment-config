697531e2d5b8dc35632c75520bb33d882ea67e5c
Michael Pratt <commits@michael-pratt.com>
2016-09-06 23:43:26 -0500

    Add external mount points

--- setup.sh
+++ setup.sh
@@ -13,30 +13,44 @@ LOCATION=$(realpath $(dirname $0))
 mkdir -p ~/.bin/
 [ ! -e "${HOME}/.ssh/config" ] && mkdir -p ${HOME}/.ssh/ && touch ${HOME}/.ssh/config
 
-# Modify some /etc stuff
-if [ -w "/etc/" ]; then
+if [ -w "/etc/inetd.conf" ]; then
+    echo "Comment all inetd.conf services"
+    sed -i 's/^#//g;s/^/#/g' /etc/inetd.conf
+    echo ""
+fi
 
-    if [ -f "/etc/inetd.conf" ]; then
-        echo "Comment all inetd.conf services"
-        sed -i 's/^#//g;s/^/#/g' /etc/inetd.conf
-        echo ""
-    fi
+if [ -w "/etc/fstab" ]; then
+    [ -w "/mnt" ] && mkdir -p /mnt/storage && mkdir -p /mnt/share && mkdir -p /mnt/windows
+    echo "Adding External Mount Points to /etc/fstab"
 
-    if [ -z "$(egrep -i '^ ?All' /etc/hosts.deny)" ]; then
-        echo "Closing hosts.deny"
-        sed -i 's/^#//g;s/^/#/g' /etc/hosts.deny
-        sed -i "\$i All: All"  /etc/hosts.deny
-    fi
+    FSTABLINES=( \
+        'UUID=687320772252F94A /mnt/share fuseblk rw,nosuid,nodev,allow_other,default_permissions,blksize=4096 0 0' \
+        'UUID=64e868cc-05f2-4096-a321-d5af6b36eb8b /mnt/storage ext4 rw,nodev,nosuid,uhelper=udisks2 0 0' \
+        'UUID=687320772252F94A /mnt/windows fuseblk ro,nosuid,nodev,allow_other,default_permissions 0 0' \
+    )
 
-    LINES=( 'sshd:192.168.0.,192.168.1.,10.42.0.,127.0.0.1' 'httpd:127.0.0.1' )
-    for l in ${LINES[@]}; do
-        if [ -z $(grep -i ${l} /etc/hosts.allow) ]; then
-            echo "Adding line ${l} to /etc/hosts.allow"
-            sed -i "\$i ${l}"  /etc/hosts.allow
+    for fsl in ${FSTABLINES[@]}; do
+        if [ -z $(grep -i ${fsl} /etc/fstab) ]; then
+            echo "Adding line ${fsl} to /etc/fstab"
+            sed -i "\$i ${fsl}"  /etc/fstab
         fi
     done
-
+    echo ""
 fi
+
+if [ -z "$(egrep -i '^ ?All' /etc/hosts.deny)" ] && [ -w "/etc/hosts.deny" ]; then
+    echo "Closing hosts.deny"
+    sed -i 's/^#//g;s/^/#/g' /etc/hosts.deny
+    sed -i "\$i All: All"  /etc/hosts.deny
+fi
+
+LINES=( 'sshd:192.168.0.,192.168.1.,10.42.0.,127.0.0.1' 'httpd:127.0.0.1' )
+for l in ${LINES[@]}; do
+    if [ -z $(grep -i ${l} /etc/hosts.allow) ] && [ -w "/etc/hosts.allow" ]; then
+        echo "Adding line ${l} to /etc/hosts.allow"
+        sed -i "\$i ${l}"  /etc/hosts.allow
+    fi
+done
 
 HOSTS=( 'adrastea' 'parsiphae' )
 for h in ${HOSTS[@]}; do
