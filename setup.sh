f7d1cc6c51df8a159708098482f72792b80f0446
Michael Pratt <commits@michael-pratt.com>
2016-09-06 23:51:38 -0500

    keep the fstab stuff simple man

--- setup.sh
+++ setup.sh
@@ -19,22 +19,10 @@ if [ -w "/etc/inetd.conf" ]; then
     echo ""
 fi
 
-if [ -w "/etc/fstab" ]; then
+if [ -w "/etc/fstab" ] &&  [ -z "$(grep -i '64e868cc-05f2-4096-a321-d5af6b36eb8b' /etc/fstab)" ]; then
     [ -w "/mnt" ] && mkdir -p /mnt/storage && mkdir -p /mnt/share && mkdir -p /mnt/windows
     echo "Adding External Mount Points to /etc/fstab"
-
-    FSTABLINES=( \
-        'UUID=687320772252F94A /mnt/share fuseblk rw,nosuid,nodev,allow_other,default_permissions,blksize=4096 0 0' \
-        'UUID=64e868cc-05f2-4096-a321-d5af6b36eb8b /mnt/storage ext4 rw,nodev,nosuid,uhelper=udisks2 0 0' \
-        'UUID=687320772252F94A /mnt/windows fuseblk ro,nosuid,nodev,allow_other,default_permissions 0 0' \
-    )
-
-    for fsl in ${FSTABLINES[@]}; do
-        if [ -z $(grep -i ${fsl} /etc/fstab) ]; then
-            echo "Adding line ${fsl} to /etc/fstab"
-            sed -i "\$i ${fsl}"  /etc/fstab
-        fi
-    done
+    cat ${LOCATION}/config/fstab >> /etc/fstab
     echo ""
 fi
 
